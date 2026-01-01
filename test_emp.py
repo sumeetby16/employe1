5115c0a6b973a19cd812dfceff5a150b1f1b4481
sumeetby16 <sumeetby16>
2026-01-01 13:05:38 +0530

    save

---
+++ test_emp.py
@@ -0,0 +1,9 @@
+from emp import employee_details
+def test_employee_details():
+    output=(
+        "Employee_Name:Alice\n"
+        "Employee_ID:E1001\n"
+        "Department:IT\n"
+        "Salary:55000"
+    )
+    assert employee_details("Alice","E1001","IT",55000) == output
